d914b2fcf27fc7977ff252b2696a28469c7face9
Charles <charlescastillo1982@yahoo.com>
2015-05-04 20:45:56 -0400

    Initial commit

--- models.rb
+++ models.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+validates_presence_of :name 
+validates_presence_of :email
+validates_presence_of :password
+     end
+  end
+end
+
+if database.table_exists?('users')
+#do something
+else 
+	raise "The table 'users' doesnt exist."
 end	
